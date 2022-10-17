5de7019171c389dfd4010707a492f0b0e5008525
Rohit Anabhavane <rohitanabhavane7@gmail.com>
2022-10-17 21:08:22 +0530

    17/10/2022

---
+++ app/src/main/java/com/rohitanabhavane/coachsir/Activities/NoConnectionActivity.java
@@ -0,0 +1,16 @@
+package com.rohitanabhavane.coachsir.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rohitanabhavane.coachsir.R;
+
+public class NoConnectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_connection);
+    }
+}
